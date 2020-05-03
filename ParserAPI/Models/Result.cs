959160eba67b99bae0cd615d8d43650c9957e60d
Ajiyba Nesij <ajiybanesij@hotmail.com>
2020-05-03 17:57:46 +0300

    Web Api Project Added

---
+++ ParserAPI/Models/Result.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ParserAPI.Models
+{
+    public class Result<T>
+    {
+        public bool IsSuccess { get; set; }
+        public List<T> Content { get; set; }
+        public Object Detail { get; set; }
+       
+        public readonly string Success = "Success";
+        
+    }
+}
